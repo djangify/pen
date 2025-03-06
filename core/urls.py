d24edbde4618d22d815c7bdd6fe3303b1f6d6ef8
Diane Corriette <dcorriette@gmail.com>
2025-03-06 13:03:00 +0000

    create notebook template

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     # add other paths for privacy policy, about page, etc.
     # path('policy/privacy-policy/', views.privacy_policy, name='privacy_policy'),
-    # path('about/', views.about, name='about'),
+    path('about/', views.about, name='about'),
+    path('paperback_notebook', views.notebook, name='notebook' )
 ]
