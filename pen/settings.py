53a27c3a8735449dfcc36a4cf30ec38a6ce3d4bb
Diane Corriette <dcorriette@gmail.com>
2025-03-07 20:17:41 +0000

    Update site for production. Site up and working

--- pen/settings.py
+++ pen/settings.py
@@ -121,13 +121,12 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
