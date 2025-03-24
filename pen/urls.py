2656143e41229e1262cbd1757828585caf814565
Diane Corriette <dcorriette@gmail.com>
2025-03-24 15:47:58 +0000

    Create accounts app and add models, views, templates and urls

--- pen/urls.py
+++ pen/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')), 
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('prompt/', include('prompt.urls')), 
     path("blog/", include("blog.urls", namespace="blog")),
 ]
