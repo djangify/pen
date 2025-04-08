feb1d81eaedda62b22b99489bb40da40db40fb50
Diane Corriette <dcorriette@gmail.com>
2025-04-08 12:34:19 +0100

    Update policy area. Add help and support plus cookies policies. Include index for policies and breadcrumb

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('paperback_notebook', views.notebook, name='notebook' ),
     path('policy/privacy-policy/', views.privacy_policy, name='privacy_policy'),
     path('policy/advertising-policy/', views.advertising_policy, name='advertising_policy'),
+    path('policy/cookies-policy/', views.cookies_policy, name='cookies_policy'),
+    path('policy/support-policy/', views.support_policy, name='support_policy'),
     path('policy/terms-conditions/', views.terms_conditions, name='terms_conditions'),
+    path('policy/', views.policies_index, name='policies_index'),
     path('robots.txt', views.robots_txt, name='robots_txt'),
 ]
