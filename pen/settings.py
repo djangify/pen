6e8e39a8d67a6268256438704c5757c64c86c020
Diane Corriette <dcorriette@gmail.com>
2025-04-26 11:53:23 +0100

    Add wp_redirects file to redirect old wordpress files

--- pen/settings.py
+++ pen/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pen.wp_redirects.EnhancedWordPressRedirectMiddleware',
 ]
 
 ROOT_URLCONF = 'pen.urls'
