55e619c52b599c5cc0de9da65a908d16dc311aa4
djangify <djangify@gmail.com>
2025-05-26 12:22:56 +0100

    Update change from MariaDB to Postgresql

--- pen/settings.py
+++ pen/settings.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 import environ
 import os
-import pymysql 
+from decouple import config
 
-pymysql.install_as_MySQLdb()
 
 # Initialize environment variables
 env = environ.Env()
@@ -20,15 +19,15 @@ environ.Env.read_env(os.path.join(BASE_DIR, ENV_FILE))
 
 SITE_ID = 1
 
-SITE_URL = 'www.penandipublishing.co.uk'
+SITE_URL = 'www.penandipublishing.com'
 
 SECRET_KEY = env("SECRET_KEY", default="your-secret-key-here")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['65.108.89.200','test.penandipublishing.com', 'localhost','127.0.0.1',]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost",
-    "http://127.0.0.1",
+    "https://65.108.89.200",
+    "https://test.penandipublishing.com",
 ]
 
 # Application definition
@@ -87,21 +86,14 @@ WSGI_APPLICATION = 'pen.wsgi.application'
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": env("DATABASE_NAME"),
-        "USER": env("DATABASE_USER"),
-        "PASSWORD": env("DATABASE_PASSWORD"),
-        "HOST": env("DATABASE_HOST", default="127.0.0.1"),
-        "PORT": env("DATABASE_PORT", default="3306"),
-        "OPTIONS": {
-            "charset": "utf8mb4",
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-            "use_unicode": True,
-            "connect_timeout": 10,
-            "autocommit": True,
-        },
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('DATABASE_PORT', default='5432'),
+    }
 }
 
 
@@ -179,9 +171,19 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 CONTACT_EMAIL = env('CONTACT_EMAIL')
 
+# Security settings
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 # IndexNow configuration
-SITE_URL = os.getenv('SITE_URL', 'https://www.penandipublishing.co.uk')
+SITE_URL = os.getenv('SITE_URL', 'https://www.penandipublishing.com')
 INDEXNOW_KEY = os.environ['INDEXNOW_KEY']  # will blow up early if missing
 
 # Which models to auto-ping via signals
