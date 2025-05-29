b9538f4d5ae8ac02c27704c54b836b69709c1907
djangify <djangify@gmail.com>
2025-05-29 19:42:20 +0100

    Update for production to vps hosting

--- pen/settings.py
+++ pen/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import environ
 import os
-from decouple import config
 
 
 # Initialize environment variables
@@ -11,7 +10,7 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Load the .env file
 ENV_FILE = ".env"
@@ -19,116 +18,126 @@ environ.Env.read_env(os.path.join(BASE_DIR, ENV_FILE))
 
 SITE_ID = 1
 
-SITE_URL = 'www.penandipublishing.com'
+SITE_URL = "www.penandipublishing.com"
 
 SECRET_KEY = env("SECRET_KEY", default="your-secret-key-here")
 
-ALLOWED_HOSTS = ['65.108.89.200','test.penandipublishing.com', 'localhost','127.0.0.1',]
+ALLOWED_HOSTS = [
+    "65.108.89.200",
+    "www.penandipublishing.com",
+    "localhost",
+    "127.0.0.1",
+    "penandipublishing.com",
+    "penandipublishing.co.uk",
+    "www.penandipublishing.co.uk",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://65.108.89.200",
-    "https://test.penandipublishing.com",
+    "https://www.penandipublishing.com",
+    "https://penandipublishing.com",
+    "https://penandipublishing.co.uk",
+    "https://www.penandipublishing.co.uk",
 ]
+
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST", default="localhost"),
+        "PORT": env("DATABASE_PORT", default="5432"),
+    }
+}
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.sites',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sitemaps',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'tinymce', 
-    'prompt',
-    'accounts',
-    'blog',
-    'core',
-    'redirects',
+    "django.contrib.sites",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sitemaps",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "tinymce",
+    "prompt",
+    "accounts",
+    "blog",
+    "core",
+    "redirects",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'redirects.middleware.RedirectMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pen.wp_redirects.WordPressRedirectMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "redirects.middleware.RedirectMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "pen.wp_redirects.WordPressRedirectMiddleware",
 ]
 
-ROOT_URLCONF = 'pen.urls'
+ROOT_URLCONF = "pen.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'js_asset.context_processors.importmap', 
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "js_asset.context_processors.importmap",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'pen.wsgi.application'
-
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST', default='localhost'),
-        'PORT': config('DATABASE_PORT', default='5432'),
-    }
-}
-
+WSGI_APPLICATION = "pen.wsgi.application"
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # WhiteNoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # WhiteNoise configuration (if you're using WhiteNoise)
 WHITENOISE_USE_FINDERS = True
@@ -138,37 +147,38 @@ WHITENOISE_MANIFEST_STRICT = False
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # REST Framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 # Authentication backends to include email login
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'accounts.authentication.EmailAuthBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "accounts.authentication.EmailAuthBackend",
 ]
 
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'  # Using local mail server
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "localhost"  # Using local mail server
 EMAIL_PORT = 25  # Standard SMTP port
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
-CONTACT_EMAIL = env('CONTACT_EMAIL')
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+CONTACT_EMAIL = env("CONTACT_EMAIL")
 
 # Security settings
 SECURE_SSL_REDIRECT = False
@@ -176,52 +186,48 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-X_FRAME_OPTIONS = 'DENY'
+X_FRAME_OPTIONS = "DENY"
 SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
 
 # For development only
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Password reset settings
 PASSWORD_RESET_TIMEOUT = 86400  # 24 hours in seconds
 
-# Add TinyMCE configuration 
+# Add TinyMCE configuration
 TINYMCE_DEFAULT_CONFIG = {
-    'height': 660,
-    'width': 'auto',
-    'forced_root_block': 'p',
-    'remove_trailing_brs': False,
-    'end_container_on_empty_block': True,
-    'formats': {
-        'p': { 'block': 'p', 'styles': { 'margin-bottom': '1.5em' } }
-    },
-    'cleanup_on_startup': True,
-    'custom_undo_redo_levels': 20,
-    'selector': 'textarea',
-    'theme': 'silver',
-    'plugins': '''
+    "height": 660,
+    "width": "auto",
+    "forced_root_block": "p",
+    "remove_trailing_brs": False,
+    "end_container_on_empty_block": True,
+    "formats": {"p": {"block": "p", "styles": {"margin-bottom": "1.5em"}}},
+    "cleanup_on_startup": True,
+    "custom_undo_redo_levels": 20,
+    "selector": "textarea",
+    "theme": "silver",
+    "plugins": """
         textcolor save link image media preview codesample contextmenu
         table code lists fullscreen insertdatetime nonbreaking
         contextmenu directionality searchreplace wordcount visualblocks
         visualchars code fullscreen autolink lists charmap print hr
         anchor pagebreak
-        ''',
-    'toolbar1': '''
+        """,
+    "toolbar1": """
         fullscreen preview bold italic underline | fontselect,
         fontsizeselect | forecolor backcolor | alignleft alignright |
         aligncenter alignjustify | indent outdent | bullist numlist table |
         | link image media | codesample |
-        ''',
-    'toolbar2': '''
+        """,
+    "toolbar2": """
         visualblocks visualchars |
         charmap hr pagebreak nonbreaking anchor | code |
-        ''',
-    'contextmenu': 'formats | link image',
-    'menubar': True,
-    'statusbar': True,
-    
+        """,
+    "contextmenu": "formats | link image",
+    "menubar": True,
+    "statusbar": True,
 }
-
--- prompt/views.py
+++ prompt/views.py
@@ -3,55 +3,63 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import Tag, PromptCategory, WritingPrompt
-from .serializers import TagSerializer, PromptCategorySerializer, WritingPromptSerializer
+from .serializers import (
+    TagSerializer,
+    PromptCategorySerializer,
+    WritingPromptSerializer,
+)
 import random
+
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [permissions.AllowAny]
 
+
 class PromptCategoryViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = PromptCategory.objects.all().order_by('name')
+    queryset = PromptCategory.objects.all().order_by("name")
     serializer_class = PromptCategorySerializer
     permission_classes = [permissions.AllowAny]
 
-class WritingPromptViewSet(viewsets.ReadOnlyModelViewSet):  # Changed to ReadOnlyModelViewSet
+
+class WritingPromptViewSet(
+    viewsets.ReadOnlyModelViewSet
+):  # Changed to ReadOnlyModelViewSet
     queryset = WritingPrompt.objects.filter(active=True)
     serializer_class = WritingPromptSerializer
     permission_classes = [permissions.AllowAny]  # Changed to AllowAny
-    
+
     def get_queryset(self):
         """
         Allow filtering by query parameters
         """
         queryset = WritingPrompt.objects.filter(active=True)
-        
+
         # Get query parameters
-        category_slug = self.request.query_params.get('category__slug', None)
-        difficulty = self.request.query_params.get('difficulty', None)
-        prompt_type = self.request.query_params.get('prompt_type__in', None)
-        
+        category_slug = self.request.query_params.get("category__slug", None)
+        difficulty = self.request.query_params.get("difficulty", None)
+        prompt_type = self.request.query_params.get("prompt_type__in", None)
+
         # Apply filters if provided
         if category_slug:
             queryset = queryset.filter(category__slug=category_slug)
-        
+
         if difficulty:
             queryset = queryset.filter(difficulty=difficulty)
-        
+
         if prompt_type:
-            prompt_types = prompt_type.split(',')
+            prompt_types = prompt_type.split(",")
             queryset = queryset.filter(prompt_type__in=prompt_types)
-            
+
         return queryset
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([permissions.AllowAny])
 def random_prompt(request):
     """
@@ -59,50 +67,50 @@ def random_prompt(request):
     tracking previously shown prompts via session to avoid repetition.
     """
     # Get filter parameters
-    category_slug = request.query_params.get('category', None)
-    difficulty = request.query_params.get('difficulty', None)
-    prompt_type = request.query_params.get('type', None)
-    
+    category_slug = request.query_params.get("category", None)
+    difficulty = request.query_params.get("difficulty", None)
+    prompt_type = request.query_params.get("type", None)
+
     # Get current prompt ID to exclude (for "Get Another Prompt")
-    current_prompt_id = request.query_params.get('current_id', None)
-    
+    current_prompt_id = request.query_params.get("current_id", None)
+
     # Start with all active prompts
     prompts = WritingPrompt.objects.filter(active=True)
-    
+
     # Apply filters if provided
-    if category_slug and category_slug != '':
+    if category_slug and category_slug != "":
         prompts = prompts.filter(category__slug=category_slug)
-    
-    if difficulty and difficulty != '':
+
+    if difficulty and difficulty != "":
         prompts = prompts.filter(difficulty=difficulty)
-    
-    if prompt_type and prompt_type != '' and prompt_type != 'both':
-        prompts = prompts.filter(prompt_type__in=[prompt_type, 'both'])
-    
+
+    if prompt_type and prompt_type != "" and prompt_type != "both":
+        prompts = prompts.filter(prompt_type__in=[prompt_type, "both"])
+
     # Exclude current prompt if getting another one
     if current_prompt_id and current_prompt_id.isdigit():
         prompts = prompts.exclude(id=int(current_prompt_id))
-    
+
     # Initialize the session if it doesn't exist
     if not request.session.session_key:
         request.session.save()
-    
+
     # Get shown prompts from session
     session_key = f"shown_prompts_{category_slug or 'all'}_{difficulty or 'all'}_{prompt_type or 'all'}"
     shown_prompts = request.session.get(session_key, [])
-    
+
     # Create a working copy of the queryset before applying session-based filtering
     available_prompts = prompts
-    
+
     # If we have shown prompts and there are enough prompts to filter
     # Only exclude previously shown prompts if we still have plenty to show
     if shown_prompts and available_prompts.count() > len(shown_prompts) / 2:
         # Create a list of IDs to exclude
         prompts_to_exclude = [id for id in shown_prompts if isinstance(id, int)]
-        
+
         # Apply the exclusion filter
         available_prompts = available_prompts.exclude(id__in=prompts_to_exclude)
-    
+
     # Check if we have any prompts after all filtering
     if not available_prompts.exists():
         # If we've excluded all prompts based on session history, reset and use all prompts
@@ -116,33 +124,35 @@ def random_prompt(request):
                 # If we're trying to get another prompt but none are available
                 response_data = {
                     "message": "You've seen all prompts matching these filters. Try different options.",
-                    "no_more_prompts": True
+                    "no_more_prompts": True,
                 }
                 return Response(response_data, status=200)
             else:
                 # No prompts at all with these filters
                 response_data = {
                     "message": "No prompts found with these filters. Try different options.",
-                    "no_prompts": True
+                    "no_prompts": True,
                 }
                 return Response(response_data, status=200)
-    
+
     # Select a random prompt
     prompt = random.choice(list(available_prompts))
-    
+
     # Serialize the prompt
     serializer = WritingPromptSerializer(prompt)
     response_data = serializer.data
-    
+
     # Add a flag indicating this is a valid prompt response
-    response_data['is_valid_prompt'] = True
-    
+    response_data["is_valid_prompt"] = True
+
     # Add is_favourite field if user is authenticated
     if request.user.is_authenticated:
-        response_data['is_favourite'] = prompt in request.user.profile.favourite_prompts.all()
+        response_data["is_favourite"] = (
+            prompt in request.user.profile.favourite_prompts.all()
+        )
     else:
-        response_data['is_favourite'] = False
-    
+        response_data["is_favourite"] = False
+
     # Add to shown prompts
     if prompt.id not in shown_prompts:
         shown_prompts.append(prompt.id)
@@ -152,7 +162,7 @@ def random_prompt(request):
         request.session[session_key] = shown_prompts
         # Save session explicitly to ensure it's stored
         request.session.modified = True
-    
+
     return Response(response_data)
 
 
@@ -160,44 +170,41 @@ def random_prompt(request):
 def add_favourite_prompt(request, prompt_id):
     prompt = get_object_or_404(WritingPrompt, id=prompt_id)
     user_profile = request.user.profile
-    
+
     # Check if the prompt is already in favorites
     is_favourite = prompt in user_profile.favourite_prompts.all()
-    
+
     # Toggle the favorite status
     if is_favourite:
         user_profile.favourite_prompts.remove(prompt)
         is_favourite = False
-        message = 'Prompt removed from your profile.'
+        message = "Prompt removed from your profile."
     else:
         user_profile.favourite_prompts.add(prompt)
         is_favourite = True
-        message = 'Prompt added to your profile!'
-    
+        message = "Prompt added to your profile!"
+
     # If the request is AJAX, return a JSON response
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        return JsonResponse({
-            'status': 'success',
-            'is_favourite': is_favourite,
-            'message': message
-        })
-    
+    if request.headers.get("x-requested-with") == "XMLHttpRequest":
+        return JsonResponse(
+            {"status": "success", "is_favourite": is_favourite, "message": message}
+        )
+
     # Otherwise redirect back to referring page
     messages.success(request, message)
-    return redirect(request.META.get('HTTP_REFERER', 'core:home'))
+    return redirect(request.META.get("HTTP_REFERER", "core:home"))
 
 
 def journal_prompt_generator(request):
     """
     Dedicated page for journal prompt generator
     """
-    return render(request, 'prompt/journal_prompt_generator.html')
+    return render(request, "prompt/journal_prompt_generator.html")
 
 
 def prompt_category_view(request, slug):
     category = get_object_or_404(PromptCategory, slug=slug)
     prompts = WritingPrompt.objects.filter(category=category, active=True)
-    return render(request, 'prompt/category.html', {
-        'category': category,
-        'prompts': prompts
-    })
+    return render(
+        request, "prompt/category.html", {"category": category, "prompts": prompts}
+    )
